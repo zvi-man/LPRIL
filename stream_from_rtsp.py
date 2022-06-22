fae9aedd21b56b396ca1401fa0709d383de010e9
Zvi <zvymendel@gmail.com>
2022-06-22 09:35:06 +0300

    update gitignore

--- stream_from_rtsp.py
+++ stream_from_rtsp.py
@@ -4,19 +4,33 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
+# Constants
+RTSP_CAM = r"rtsp://10.100.102.20:8554/mjpeg/1"
+VIDEO = r"videos/Home Invasion Caught on 16 Surveillance Cameras.mp4"
+WEB_CAM = 0
+NUM_OF_FRAMES_TO_SKIP = 1
+PLT_DELAY_TIME = 0.02
+
+
 def main():
-    vidcap = cv2.VideoCapture(r"rtsp://10.100.102.20:8554/mjpeg/1")
+    image_count = 0
+    vidcap = cv2.VideoCapture(WEB_CAM)
     success, image = vidcap.read()
     old_time = datetime.now()
     ax = plt.imshow(image)
     plt.ion()
     while success:
         success, image = vidcap.read()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_count += NUM_OF_FRAMES_TO_SKIP
+        vidcap.set(cv2.CAP_PROP_POS_FRAMES, image_count)
         frame_delay = datetime.now() - old_time
         old_time = datetime.now()
         ax.set_data(image)
         plt.title(f"frame delay: {frame_delay}")
-        plt.pause(0.02)
+        plt.pause(PLT_DELAY_TIME)
+
+    plt.ioff()
 
 
 if __name__ == '__main__':
