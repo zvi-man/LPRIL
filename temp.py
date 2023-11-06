f005ea514deb2057be31c9d33305b988909843d1
ZviLinux <zvymendel@gmail.com>
2023-11-06 17:10:24 +0200

    Added combine_df_scores function

--- temp.py
+++ temp.py
@@ -20,6 +20,11 @@ def get_emb_by_direction(embeddings: np.ndarray, file_paths: List[str]) -> Tuple
     return embeddings[indexes_front], embeddings[indexes_back]
 
 
+def combine_df_scores(df: pd.DataFrame, columns_to_merge: List[str], new_col_name: str) -> pd.DataFrame:
+    df[new_col_name] = df.loc[:, columns_to_merge].prod(axis=1)
+    return df
+
+
 def example_split_embeddings_by_path():
     emb = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
     file_paths = ["aaa/1.jpg", "aaa/2.jpg", "aaa/3.jpg", "aaa/4.jpg"]
@@ -28,5 +33,15 @@ def example_split_embeddings_by_path():
     print(f"back embeddings: {back_emb}")
 
 
+def example_combine_df_scores():
+    df = pd.DataFrame({'score': [0.1, 0.2, 0.3, 0.4], 'color_true': [True, True, False, False],
+                       'maker_true': [True, False, True, False]})
+    combine_df_scores(df, ['score', 'color_true', 'maker_true'], 'RCM')
+    combine_df_scores(df, ['score', 'color_true'], 'RC')
+    combine_df_scores(df, ['score', 'maker_true'], 'RM')
+    print(df)
+
+
 if __name__ == '__main__':
-    example_split_embeddings_by_path()
+    example_combine_df_scores()
+    # example_split_embeddings_by_path()
